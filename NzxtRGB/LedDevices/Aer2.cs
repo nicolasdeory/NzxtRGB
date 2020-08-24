ea2af18cb8d5dc91845bc50e70304cd08c24bc31
Nicolás de Ory <deorynicolas@gmail.com>
2020-08-25 00:15:47 +0200

    Changed accessibility

--- NzxtRGB/LedDevices/Aer2.cs
+++ NzxtRGB/LedDevices/Aer2.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace NzxtRGB.LedDevices
 {
-    class Aer2 : NzxtLedDevice
+    public class Aer2 : NzxtLedDevice
     {
         public override string Name => "Aer 2";
         public override int LedCount => 16; // 8 LED x 2 fans
